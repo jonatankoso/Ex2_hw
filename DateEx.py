4c4d32c58a8ebfbef05b17faeb3a5cf48298c1da
Jonatan Kosower <jonatankoso@gmail.com>
2023-12-23 18:12:29 +0200

    initialized date class

---
+++ DateEx.py
@@ -0,0 +1,13 @@
+class Date(object):
+    def __init__(self, date):
+        self.day, self.month, self.year = date.split(".")
+
+    def set_day(self, new_day: str):
+        self.day = new_day
+
+    def set_month(self, new_month: str):
+        self.month = new_month
+
+    def set_year(self, new_year: str):
+        self.year = new_year
+

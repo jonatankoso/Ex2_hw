0a7c7f4387902c7c2908aa50436e77af8f0899fd
Jonatan Kosower <jonatankoso@gmail.com>
2023-12-23 16:23:40 +0200

    added init to talker class and talk function

--- Talkers.py
+++ Talkers.py
@@ -1,10 +1,12 @@
 class Person(object):
-    def __int__(self, fname, lname, age):
+    def __init__(self, fname: str, lname: str, age: int):
+        self.fname = fname
         if age <= 0:
             print("age cannot be negative or 0, age set to 1")
             self.age = 1
         else:
             self.age = age
+
         self.fname = fname
         self.lname = lname
 
@@ -17,5 +19,15 @@ class Person(object):
     def set_age(self, new_age):
         self.age = new_age
 
-class Talker(object):
-    def __int__(self):
+
+class Talker(Person):
+    def __int__(self, fname: str, lname: str, age: int):
+        super.__init__(fname, lname, age)
+
+    def talk(self, text: str):
+        print(text)
+
+
+if __name__ == "__main__":
+    t1 = Talker("may", "cohen", 19)
+    t1.talk("hey")

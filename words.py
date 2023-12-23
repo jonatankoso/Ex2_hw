29d4560631014b928617172cd2dec75067949573
Jonatan Kosower <jonatankoso@gmail.com>
2023-12-23 17:18:29 +0200

    added words_appear function

--- words.py
+++ words.py
@@ -15,5 +15,30 @@ def word_count(file_path):
         print(f"Error reading the file '{file_path}'.")
 
 
+def word_appear(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+            # Split the content into words
+            wordsList = content.split()
+
+            wordsDict = {}
+            for word in wordsList:
+                if(word not in wordsDict.keys()):
+                    wordsDict.update({word : 1})
+                else:
+                    wordsDict[word] = wordsDict[word] + 1
+
+            for key, value in wordsDict.items():
+                print(f"{key}: {value}")
+
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except IOError:
+        print(f"Error reading the file '{file_path}'.")
+
 if __name__ == "__main__":
-    word_count(r"C:\Users\magshimim\Documents\Omega Projects\Python\lesson2\one.txt")
+    path = r"C:\Users\magshimim\Documents\Omega Projects\Python\lesson2\one.txt"
+    word_count(path)
+    word_appear(path)
